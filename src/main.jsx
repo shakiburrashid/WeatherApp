a2f61dee3814a2d6d1ec2b09e73a801ecde8e486
shakiburrashid <shakiburrashid7@gmail.com>
2025-07-06 03:24:35 +0600

    api connect

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
+// import WeatherApi from './WeatherApi'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* <WeatherApi/> */}
   </StrictMode>,
 )
